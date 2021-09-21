0d83a2d452c0848aecc68e23869a0918649d7d6a
Aysel <ayselk94@gmail.com>
2021-09-20 22:59:18 -0400

    Added information into READ ME; added data cleaned up some code

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 ProjectFoodRoulette
+
+BASIC FUNTIONALITY 
+
+1. User opens up the application 
+
+2. User clicks on 'food randomizer' button, which will then provide them a type of cuisine/food type 
+
+3. After selecting the food type - then the user can select specific resturants following that food type 
+
+4. When the list of resturants are returned - the user will also view additional information such as:
+  - Resturant Website 
+  - Popular meals/items from the resturant 
+  - Links to order services (grubhub, doordash, ubereats)
+
+//////////////////////////////////////////////////
+
+ADDITIONAL FEATURES:
+
+*Saved history of previously selected resturants 
+
+*Itemized saving from resturants (e.g. having a saved page in user profile where the user can 'favorite' an item from a resturant)
+
+*Map/GPS functionality 
+
+*Meal Reporting
+
+*Sharing resturants and favorites 
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Array to store the food types 
 let foodTypes = [
   'Fast Food',
   'Thai Food',
@@ -12,12 +13,29 @@ let foodTypes = [
   'Chinese Food',
 ];
 
-let randomFood = foodTypes[Math.trunc(Math.random() * foodTypes.length)];
-
+//Configuring the random food button 
 const ranFoodBtn = document.querySelector('.ran--Food--Btn');
 
+//DOM for food button 
 ranFoodBtn.addEventListener('click', function () {
-  console.log(foodTypes);
+  let randomFood = foodTypes[Math.trunc(Math.random() * foodTypes.length)];
+  console.log(randomFood);
+  
 });
+
+//Array for each food type to select options/resturants 
+const fastFood = ['Wendy\'s', 'Tacobell', 'McDonald\'s', 'Subway', 'Chick-fila', 'Sonic', 'Chipolte', 'Dominoes'];
+const thaiFood = [];
+const turkishFood = [];
+const mexicanFood = [];
+const spanishFood = [];
+const italianFood = [];
+const frenchFood = [];
+const japaneseFood = [];
+const chineseFood = [];
+
+
+
+
 
 
