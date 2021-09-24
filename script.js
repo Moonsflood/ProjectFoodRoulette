32069a42d6fc18958d5adbc0947dbb9b60df6e60
Aysel <ayselk94@gmail.com>
2021-09-24 16:08:18 -0400

    Added food type randomizer on button click to display the food type

--- script.js
+++ script.js
@@ -15,17 +15,26 @@ let foodTypes = [
 
 //Configuring the random food button 
 const ranFoodBtn = document.querySelector('.ran--Food--Btn');
+//const ranFoodBtn1 = document.getElementById('ran--Food--Btn1');
 
 //DOM for food button 
-ranFoodBtn.addEventListener('click', function () {
+/*ranFoodBtn.addEventListener('click', function () {
   let randomFood = foodTypes[Math.trunc(Math.random() * foodTypes.length)];
   console.log(randomFood);
-  
-});
+});*/
+
+
+//If a resturant type is selected, then loop through random resutrants of the type that has been displayed 
+
+function foodButton() {
+  let randomFood = foodTypes[Math.trunc(Math.random() * foodTypes.length)];
+  console.log(randomFood);
+  document.querySelector(".randomFood").innerHTML = randomFood;
+}
 
 //Array for each food type to select options/resturants 
 const fastFood = ['Wendy\'s', 'Tacobell', 'McDonald\'s', 'Subway', 'Chick-fila', 'Sonic', 'Chipolte', 'Dominoes'];
-const thaiFood = [];
+const thaiFood = ['Thai', 'Thai1', 'Thai2', ];
 const turkishFood = [];
 const mexicanFood = [];
 const spanishFood = [];
